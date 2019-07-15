3318c7575f76b9cb1e24f6ae1af9cb44de1cb32d
joson jiang <joson1205@163.com>
2019-07-15 15:44:19 +0800

    Create Config.py
    配置

---
+++ Config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date        : 2019-07-08 17:09:59
+# @Author      : Joson (joson1205@163.com)
+# @Link        : github.com/joson1205
+# @Description : 配置
+
+
+# MySQL
+mysql = {
+    "host": "host",
+    "user": "root",
+    "password": "password",
+    "port": 3306,
+    "database": "my_db",
+    "charset": "utf8"
+}
